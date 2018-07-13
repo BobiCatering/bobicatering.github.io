97b3aa4623f7b70db5872ecb9e43507da4c53c42
nikoladionisiev <36667350+nikoladionisiev@users.noreply.github.com>
2018-07-13 19:22:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                Console.WriteLine("Nick is the best");
+            }
+        }
+    }
+}
